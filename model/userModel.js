8092109fd376b148858a9c49deae77c2c816c987
MuhammadMoazzamKhan <mynameismuhammadmoazzamkhan@gmail.com>
2023-10-21 05:50:23 -0700

    created time add in User Schema

--- model/userModel.js
+++ model/userModel.js
@@ -35,6 +35,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         default: "user"
     },
+    createdAt:{
+        type: Date,
+        default:Date.now
+    },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
 });
